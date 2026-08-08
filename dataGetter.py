b5adcb3513ba32c99ed7bce6bd36374bd5cfeffa
Natnael Yohannes <natnael.m.yohannes@gmail.com>
2026-08-07 23:07:06 -0400

    Abstracted data retrieval

---
+++ dataGetter.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+
+
+def getData(stock : str, start_date : str, end_date : str) -> list[float]:
+    data = yf.download(stock, start=start_date, end=end_date, interval="1m")
+    csvData = data.to_csv()
+    lines = csvData.split("\n")
+    output : list[float] = []
+    for i in range(len(lines)):
+        # First 6 lines are headers & odd lines are blankspace seperators.
+        if i < 6 or i % 2 == 1:
+            continue
+        # We take item 2 of the comma seperated line for each datapoint line.
+        output.append(float(lines[i].split(",")[1]))
+    return output
